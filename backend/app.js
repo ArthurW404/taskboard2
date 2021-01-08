f4ad4e9e7eef531cfebe7a500858f7fcaa515f16
Arthur <ArthurWang2000@hotmail.com>
2021-01-08 21:17:49 +1100

    Added jwt to backend dependency

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = 5000;
 
+const privateKey = "test private key";
+
+app.use(express.json()); // for parsing application/json
+// app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("World!");
+});
+
+app.post("/login", (req, res) => {
+  const token = jwt.sign(req.body, privateKey);
+  console.log(token);
+
+  // test decoding
+  // const decoded = jwt.verify(token, privateKey);
+  // console.log(decoded);
+
+  // return to client jwt if login credentials are correct
+  res.json(token);
 });
 
 app.get("/getBoard", (req, res) => {
@@ -15,42 +32,22 @@ app.get("/getBoard", (req, res) => {
     {
       name: "fk",
       id: 1,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
+      image: "https://www.bizreport.com/2011/02/03/android-logo-200x200.jpg",
     },
     {
       name: "fk",
       id: 2,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
+      image: "https://www.bizreport.com/2011/02/03/android-logo-200x200.jpg",
     },
     {
       name: "fk",
       id: 3,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
+      image: "https://www.bizreport.com/2011/02/03/android-logo-200x200.jpg",
     },
     {
       name: "fk",
       id: 4,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
-    },
-    {
-      name: "fk",
-      id: 5,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
-    },
-    {
-      name: "fk",
-      id: 6,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
-    },
-    {
-      name: "fk",
-      id: 7,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
-    },
-    {
-      name: "fk",
-      id: 8,
-      image: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg",
+      image: "https://www.bizreport.com/2011/02/03/android-logo-200x200.jpg",
     },
   ];
   res.json(data);
